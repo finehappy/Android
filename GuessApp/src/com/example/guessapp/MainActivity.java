4474db33c5304c5eb0bec93b6e310d323c9b58dc
finehappy <finehappy@gmail.com>
2015-03-25 00:30:26 +1000

    change content

--- GuessApp/src/com/example/guessapp/MainActivity.java
+++ GuessApp/src/com/example/guessapp/MainActivity.java
@@ -61,11 +61,22 @@ public class MainActivity extends ActionBarActivity {
 	
 		
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if(id == R.id.action_settings){
+		if (id == R.id.action_settings) {
 			Intent intent = new Intent(this, Setting.class);
 			startActivityForResult(intent, 1);
 			return true;
@@ -83,6 +94,23 @@ public class MainActivity extends ActionBarActivity {
 			
 		}
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
 	}
 }
 
